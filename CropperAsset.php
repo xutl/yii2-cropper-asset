8b625d595f65f502322905605995e4cebaf0ecaf
xutl <79109681@qq.com>
2016-10-17 13:29:16 +0800

    c

---
+++ CropperAsset.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace xutl\cropper;
+
+use yii\web\AssetBundle;
+
+/**
+ * TypeAheadAsset
+ *
+ * @author Alexander Kochetov <creocoder@gmail.com>
+ */
+class CropperAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/xutl/yii2-cropper-widget/assets';
+
+    public $css = [
+        'cropper.min.css',
+    ];
+
+    public $js = [
+        'cropper.min.js',
+    ];
+
+    public $depends = [
+		'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+
+}
